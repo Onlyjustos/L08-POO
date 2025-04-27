22c91c0435a122131c3d78afd9956c26977385d9
João Guimaraes <joaofrederico.xavier@gmail.com>
2025-04-27 20:25:18 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,14 +1,14 @@
 public class Funcionario extends PessoaFisica{
-    int matricula;
-    double salario;
-    String cargo;
+    private int matricula;
+    private double salario;
+    private String cargo;
 
     public Funcionario(String cargo, int matricula, double salario,String cpf,char sexo,String estadoCivil,String endereco, String nome,String telefone) {
         super(cpf,sexo,estadoCivil,endereco,nome,telefone);
         this.cargo = cargo;
         this.matricula = matricula;
         this.salario = salario;
-        
+
     }
 
 }
